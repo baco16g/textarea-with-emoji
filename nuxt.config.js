119da3e4835fdc216a0a5f2d75b46d3181596042
baco16g <baco.16g@gmail.com>
2020-01-07 16:57:41 +0900

    feat: setup for Github Page

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,13 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/textarea-with-emoji/favicon.ico'
+      }
+    ]
   },
   /*
    ** Customize the progress-bar color
@@ -49,5 +55,8 @@ export default {
      ** You can extend webpack config here
      */
     extend(config, ctx) {}
+  },
+  router: {
+    base: '/textarea-with-emoji/'
   }
 }
